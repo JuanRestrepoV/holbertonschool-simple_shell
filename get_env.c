f720c604d3c91825050ab14b717047fab2a9f3d7
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-01-14 10:15:25 -0800

    Update of get_env function

--- get_env.c
+++ get_env.c
@@ -1,7 +1,12 @@
 #include "header.h"
 /**
-  */
-int *get_env(char* name)
+* get_env - Return the value of the environment variable found.
+*
+* @name: Name of the command (environ. var.) get from the tokenization.
+*
+* Return: Pointer to the value of the environment variable.
+*/
+char *get_env(char* name)
 {
 	char **environ_copy;
 
@@ -9,4 +14,15 @@ int *get_env(char* name)
 	{
 		return (NULL);
 	}
+	
+	environ_copy = environ;
+
+	while (*environ_copy != NULL)
+	{
+		if (strncmp(*environ_copy, name) == 0)
+		{
+			return (*env_var);
+		}
+	}
+	return (NULL);
 }
