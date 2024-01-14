cf3090a84589806674818c0c76975e138b5bb110
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-01-14 14:34:13 -0800

    update prototype of path_env function

--- path_env.c
+++ path_env.c
@@ -1,6 +1,11 @@
 #include "header.h"
 /**
   */
-char *pathEnv(char *command)
+char *path_env(char *command)
 {
-	char *path = get_env_value("PATH");
+	char *path, *path_copy;
+
+	path = get_env_value("PATH");
+	path_copy = strdup(path);
+
+}
